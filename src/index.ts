2a5b00e94c6b93a12c487fa6f5c33a919c5a77d1
Anurag Gupta <aguptaking@gmail.com>
2021-02-14 20:31:26 +0530

    Add config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 1,
+  green: 1,
+  blue: 1,
+};
+
+console.log(color);
+
+export default color;
